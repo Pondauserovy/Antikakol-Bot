a02802d281d43f2d19643c9a0b2b4eda530fa857
Pondauserovy <64993417+Pondauserovy@users.noreply.github.com>
2020-05-08 02:24:32 +0300

    Add files via upload

---
+++ beliy_front.bat
@@ -0,0 +1,4 @@
+echo ODEBILEVSHIY OT PEREMOGI SHWAYNOKARAS v1.0
+echo BELIY FRONT
+
+PixelPlanetBot.exe run -x 5942 -y -9811 -i beliy-front.png --defenseMode --notificationMode both --placingOrder random
---
+++ krasniy_front.bat
@@ -0,0 +1,4 @@
+echo ODEBILEVSHIY OT PEREMOGI SHWAYNOKARAS v1.0
+echo BELIY FRONT
+
+PixelPlanetBot.exe run -x 5923 -y -9552 -i krasniy-front.png --defenseMode --notificationMode sound --placingOrder random
